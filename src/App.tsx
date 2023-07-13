a37979e4bea04cdbb93794e64b3bf4b852fc873b
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-07-13 15:53:41 +0200

    Iteration 1 - Routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage'
+import { AllBeersPage } from './pages/AllBeersPage'
+import { RandomBeerPage } from './pages/RandomBeerPage'
+import { AddBeerPage } from './pages/AddBeerPage'
+import { BeersDetailPage } from './pages/BeersDetailPage'
+import { NotFoundPage } from './pages/NotFoundPage';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
 
   return (
-    <>
-    <h1>React Beers F5</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/beers" element={<AllBeersPage/>} />
+        <Route path="/random-beer" element={<RandomBeerPage/>} />
+        <Route path="/new-beer" element={<AddBeerPage/>} />
+        <Route path="/beers/:beerId" element={<BeersDetailPage/>} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
