4c2ad4a77b834f97b3d414ee2b41ada94b118239
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-07-14 09:53:02 +0200

    allbeerspage code done

--- src/pages/AllBeersPage.tsx
+++ src/pages/AllBeersPage.tsx
@@ -1,6 +1,47 @@
 
-export const AllBeersPage = () => {
-  return (
-    <div>AllBeersPage</div>
-  )
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+interface Beer {
+  id: number;
+  image_url: string;
+  name: string;
+  tagline: string;
+  contributed_by: string;
 }
+
+
+export const AllBeersPage = () => {
+  const [beers, setBeers] = useState<Beer[]>([]);
+
+  useEffect(() => {
+    const fetchBeers = async () => {
+      try {
+        const response = await axios.get('https://f5-beers-065cad3017be.herokuapp.com/beers');
+        setBeers(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchBeers();
+  }, []);
+
+  return (
+    <div>
+      <h1>All Beers</h1>
+      <ul>
+        {beers.map(beer => (
+          <li key={beer.id}>
+            <img src={beer.image_url} alt={beer.name} />
+            <h2>{beer.name}</h2>
+            <p>{beer.tagline}</p>
+            <p>Contributed by: {beer.contributed_by}</p>
+            <Link to={`/beers/${beer.id}`}>View Details</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
